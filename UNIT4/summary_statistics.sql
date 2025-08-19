d102347a224ae56d2d28537a95be3bafb4728652
YohoCodes <isaacyoho99@gmail.com>
2025-08-19 15:37:42 -0400

    committed to SQL mastery

---
+++ UNIT4/summary_statistics.sql
@@ -0,0 +1,17 @@
+-- Calculate the average budget for films released in 2010 or later
+SELECT AVG(budget) AS avg_budget
+FROM films
+WHERE release_year >= 2010;
+
+SELECT MIN(budget) AS min_budget, COUNT(budget) AS count_budget
+FROM films
+WHERE release_year = 2010;
+
+-- USING ROUND()...
+-- This rounds to the nearest whole number
+SELECT ROUND(AVG(budget)) AS avg_budget
+FROM films
+WHERE release_year = 2010;
+
+SELECT AVG(budget) AS avg_budget
+FROM films;
